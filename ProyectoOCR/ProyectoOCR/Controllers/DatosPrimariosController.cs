08218d337fb434729019c3bf0fe84592bdd57590
Diorelyon Amador <diorel_x@hotmail.com>
2018-02-07 17:47:27 -0600

    formulario de alta
    se genero la interfas de alta

---
+++ ProyectoOCR/ProyectoOCR/Controllers/DatosPrimariosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoOCR.Controllers
+{
+    public class DatosPrimariosController : Controller
+    {
+        // GET: DatosPrimarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
