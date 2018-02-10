ffdc226317064bd30eb140d1fbce9f4adfa63eb4
Diorelyon Amador <diorel_x@hotmail.com>
2018-02-09 18:22:49 -0600

    enviar datos al controlador
    envia dtos al controlador pero no funcioan bien las notificaciones

--- ProyectoOCR/ProyectoOCR/Controllers/DatosPrimariosController.cs
+++ ProyectoOCR/ProyectoOCR/Controllers/DatosPrimariosController.cs
@@ -13,5 +13,66 @@ namespace ProyectoOCR.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(string FirtsName1)
+        {
+            var cariable1 = FirtsName1;
+
+
+            if (cariable1 != null)
+                return Json("'Success':'true'");
+            else
+                return Json(String.Format("'Success':'false','Error':'Ha habido un error al insertar el registro.'"));
+
+
+        }
+
+
+
+
+
+
+
+        //[HttpPost]
+        //public ActionResult Create(string FirtsName1,string FirtsName2,string SecondName1,string SecondName2,string Curp,string Date,string Street,string OutdoorNumber, string InteriorNumber,string PostalCode,string Colony,string Delegacion,string Entidad,string Celular,string Email,string Telefon,string Entry)
+        //{
+        //    var cariable1 = FirtsName1;
+        //    var cariable2 = FirtsName2;
+        //    var cariable3 = SecondName1;
+        //    var cariable4 = SecondName2;
+        //    var cariable5 = Curp;
+        //    var cariable6 = Date;
+        //    var cariable7 = Street;
+        //    var cariable8 = OutdoorNumber;
+        //    var cariable9 = InteriorNumber;
+        //    var cariable17 = PostalCode;
+        //    var cariable10 = Colony;
+        //    var cariable11 = Delegacion;
+        //    var cariable12 = Entidad;
+        //    var cariable13 = Celular;
+        //    var cariable14 = Email;
+        //    var cariable15 = Telefon;
+        //    var cariable16 = Entry;
+            
+        //    return RedirectToAction("AdjuntarDocumento");
+        //}
+
+
+
+
+        public ActionResult AdjuntarDocumento()
+        {
+            return View();
+        }
+
+
     }
 }
